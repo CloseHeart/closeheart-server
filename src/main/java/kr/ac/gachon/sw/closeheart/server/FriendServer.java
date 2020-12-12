9c2bec8890856a4ac3c9c8161b2dc49c362af1db
SuyeonJung09 <zyzy1231@naver.com>
2020-12-12 14:33:49 +0900

    deport FriendServer package

--- src/main/java/kr/ac/gachon/sw/closeheart/server/FriendServer.java
+++ src/main/java/kr/ac/gachon/sw/closeheart/server/FriendServer.java
@@ -3,7 +3,6 @@ package kr.ac.gachon.sw.closeheart.server;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import jdk.nashorn.internal.parser.JSONParser;
 import kr.ac.gachon.sw.closeheart.server.db.DBConnect;
 import kr.ac.gachon.sw.closeheart.server.object.User;
 import kr.ac.gachon.sw.closeheart.server.util.Util;
