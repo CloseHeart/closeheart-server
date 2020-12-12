f9322ccabaabccf58a6270e1559c47c7a3598d4c
SuyeonJung09 <zyzy1231@naver.com>
2020-12-12 15:11:06 +0900

    d

--- src/main/java/kr/ac/gachon/sw/closeheart/server/api/Covid19API.java
+++ src/main/java/kr/ac/gachon/sw/closeheart/server/api/Covid19API.java
@@ -1,7 +1,9 @@
 package kr.ac.gachon.sw.closeheart.server.api;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import kr.ac.gachon.sw.closeheart.server.db.DBConnect;
 import org.json.XML;
 
 import java.io.BufferedReader;
@@ -10,18 +12,28 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static kr.ac.gachon.sw.closeheart.server.db.DBConnect.getCovid19Info;
+import static kr.ac.gachon.sw.closeheart.server.db.DBConnect.setCovid19Info;
+
 public class Covid19API {
     private static String serviceKey = "";
     private static String serviceURL = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson";
+    private static String currentDateStr = "";
+    private static String agoDateStr = "";
 
-    public static JsonObject getCovid19Data(String dateStr) throws Exception {
+    public static JsonObject getCovid19Data(LocalDate date) throws Exception {
+        currentDateStr = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        agoDateStr = date.minusDays(1).format(DateTimeFormatter.BASIC_ISO_DATE);
 
         StringBuilder urlBuilder = new StringBuilder(serviceURL);
         urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=1");
         urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=1");
-        urlBuilder.append("&" + URLEncoder.encode("startCreateDt", "UTF-8") + "=" + dateStr);
-        urlBuilder.append("&" + URLEncoder.encode("endCreateDt", "UTF-8") + "=" + dateStr);
+        urlBuilder.append("&" + URLEncoder.encode("startCreateDt", "UTF-8") + "=" + agoDateStr);
+        urlBuilder.append("&" + URLEncoder.encode("endCreateDt", "UTF-8") + "=" + currentDateStr);
 
         URL apiURL = new URL(urlBuilder.toString());
 
@@ -59,5 +71,37 @@ public class Covid19API {
             connection.disconnect();
             return null;
         }
+    }
+
+    public void getCovid19NewDecide(JsonObject object){
+        if(DBConnect.getCovid19Info(currentDateStr) == -1){
+            DBConnect.setCovid19Info(currentDateStr, getCurrentCovid19Decide(object));
+            DBConnect.setCovid19Info(agoDateStr, getAgoCovid19Decide(object));
+        }
+        else {
+
+        }
+    }
+    public int getCurrentCovid19Decide(JsonObject object){
+        JsonObject currentObj = JsonParser.parseString(object.toString()).getAsJsonObject();
+        JsonObject response = currentObj.get("response").getAsJsonObject();
+        JsonObject body = response.get("body").getAsJsonObject();
+        JsonObject items = body.get("items").getAsJsonObject();
+        JsonArray item = items.get("item").getAsJsonArray();
+
+        JsonObject curr = (JsonObject) item.get(0);
+        int currDecideCnt = curr.get("DECIDE_CNT").getAsInt();  // 오늘 확진자 수
+        return currDecideCnt;
+    }
+    public int getAgoCovid19Decide(JsonObject object){
+        JsonObject currentObj = JsonParser.parseString(object.toString()).getAsJsonObject();
+        JsonObject response = currentObj.get("response").getAsJsonObject();
+        JsonObject body = response.get("body").getAsJsonObject();
+        JsonObject items = body.get("items").getAsJsonObject();
+        JsonArray item = items.get("item").getAsJsonArray();
+
+        JsonObject ago = (JsonObject) item.get(1);
+        int agoDecideCnt = ago.get("DECIDE_CNT").getAsInt();  // 어제 확진자 수
+        return agoDecideCnt;
     }
 }
